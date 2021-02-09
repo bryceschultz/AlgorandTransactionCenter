7da58b86338e5b6343457fc5fe43471dd55f4361
bryceschultz <49401206+bryceschultz@users.noreply.github.com>
2021-02-09 18:52:37 -0500

    Update requestscript.js

--- requestscript.js
+++ requestscript.js
@@ -47,7 +47,7 @@ window.onload = function () {
   'use strict'
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   var forms = document.querySelectorAll('.needs-validation')
-
+debugger;
   // Loop over them and prevent submission
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
