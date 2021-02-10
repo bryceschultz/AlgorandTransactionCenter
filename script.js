3efaf8d9a879095b44fdbf50b7f9d39245dc85cf
bryceschultz <49401206+bryceschultz@users.noreply.github.com>
2021-02-10 10:29:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,63 @@
-    const baseServer = "https://testnet-algorand.api.purestake.io/ps2";
-    const port = "";
-    const token = {
-        "X-API-Key": "cfoNpaCzsF9xJRTOO39rF78aJRbK4fqj4W8LNv6k",
-    };
+const baseServer = "https://testnet-algorand.api.purestake.io/ps2";
+const port = "";
+const token = {
+    "X-API-Key": "cfoNpaCzsF9xJRTOO39rF78aJRbK4fqj4W8LNv6k",
+};
 
-    const algodClient = new algosdk.Algodv2(token, baseServer, port);
-    function makeNewOriginWallet() {
-        var account = algosdk.generateAccount();
-        var passphrase = algosdk.secretKeyToMnemonic(account.sk);
-        document.getElementById("walletId").innerHTML = account.addr;
-        document.getElementById("passphrase").innerHTML = passphrase;
-    }
+const algodClient = new algosdk.Algodv2(token, baseServer, port);
+function makeNewOriginWallet() {
+    var account = algosdk.generateAccount();
+    var passphrase = algosdk.secretKeyToMnemonic(account.sk);
+    document.getElementById("walletId").innerHTML = account.addr;
+    document.getElementById("passphrase").innerHTML = passphrase;
+}
 
-
+// Get the modals
+var modal = document.getElementById("myModal");
 var createwalletmodal = document.getElementById("createWalletModal");
 var createwalletbtn = document.getElementById("createWalletButton");
-
-// When the user clicks on the button, open the modal
-createwalletbtn.onclick = function() {
-	console.log('create wallet button has fired');
-	makeNewOriginWallet();
-	createwalletmodal.style.display = "block";
-}
-	
 var fundwalletbtn = document.getElementById("fundWalletButton");
 var fundwalletmodal = document.getElementById("fundWalletModal");
-	
-// When the user clicks on the button, open the modal
-fundwalletbtn.onclick = function() {
-	console.log('fund wallet button has fired');
-	fundwalletmodal.style.display = "block";
-}
-
 var plusbtn = document.getElementById("plusButton");
 var plusbtnmodal = document.getElementById("plusButtonModal");
 
 // When the user clicks on the button, open the modal
-plusbtn.onclick = function() {
-	    if (  $( this ).css( "transform" ) == 'none' ){
-		$(this).css("transform","rotate(45deg)");
-	    } else {
-		$(this).css("transform","" );
-	    }
-	if (plusbtnmodal.style.display == "block" || createwalletmodal.style.display == "block" || fundwalletmodal.style.display == "block") {
-            plusbtnmodal.style.display = "none";
-            createwalletmodal.style.display = "none";
-            fundwalletmodal.style.display = "none";
-        } else {
-            plusbtnmodal.style.display = "block";
-        }
-}
+createwalletbtn.onclick = function () {
+    makeNewOriginWallet();
+    createwalletmodal.style.display = "block";
+};
 
-  // Get the modal
-var modal = document.getElementById("myModal");
-  
-window.onclick = function(event) {
-  if (event.target == modal || event.target == plusbtnmodal || event.target == createwalletmodal || event.target == fundwalletmodal) {
-    	modal.style.display = "none";
-	plusbtnmodal.style.display = "none";
-	createwalletmodal.style.display = "none";
-	fundwalletmodal.style.display = "none";
-	  console.log('window.onclick has fired');
-	 	if (  $('#plusButton').css( "transform" ) == 'none' ){
-		console.log('$#plusButton.css if has fired');
-		$('#plusButton').css("transform","rotate(45deg)");
-	    } else {
-		$('#plusButton').css("transform","" );
-	    }
-  }
-}
+// When the user clicks on the button, open the modal
+fundwalletbtn.onclick = function () {
+    fundwalletmodal.style.display = "block";
+};
+
+// When the user clicks on the button, open the modal
+plusbtn.onclick = function () {
+    if ($(this).css("transform") == "none") {
+        $(this).css("transform", "rotate(45deg)");
+    } else {
+        $(this).css("transform", "");
+    }
+    if (plusbtnmodal.style.display == "block" || createwalletmodal.style.display == "block" || fundwalletmodal.style.display == "block") {
+        plusbtnmodal.style.display = "none";
+        createwalletmodal.style.display = "none";
+        fundwalletmodal.style.display = "none";
+    } else {
+        plusbtnmodal.style.display = "block";
+    }
+};
+
+window.onclick = function (event) {
+    if (event.target == modal || event.target == plusbtnmodal || event.target == createwalletmodal || event.target == fundwalletmodal) {
+        modal.style.display = "none";
+        plusbtnmodal.style.display = "none";
+        createwalletmodal.style.display = "none";
+        fundwalletmodal.style.display = "none";
+        if ($("#plusButton").css("transform") == "none") {
+            $("#plusButton").css("transform", "rotate(45deg)");
+        } else {
+            $("#plusButton").css("transform", "");
+        }
+    }
+};
